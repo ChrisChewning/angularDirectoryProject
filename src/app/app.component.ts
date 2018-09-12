7edfc7c1544c1d32a82426dbd3a57ca4b42cb8fe
Chris <chewning.cl@gmail.com>
2018-09-12 16:16:22 -0500

    10/27 finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component.ts'; //this comes from the export from the home.component.ts file.
+import { HomeComponent } from './home/index'; //optional way
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  directives: [HomeComponent]
 })
 export class AppComponent {
   title = 'angular-directory';
